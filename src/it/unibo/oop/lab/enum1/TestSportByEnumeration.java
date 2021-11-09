39bc805e9c99c499c22a37de7004d4ac1f00eeed
michele.ravaioli3 <michele.ravaioli3@lab22-03-02.campusfc.dir.unibo.it>
2021-11-09 16:23:14 +0100

    Completed enum1

--- src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
+++ src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.lab.enum1;
 
+import it.unibo.oop.lab.enum1.SportSocialNetworkUserImpl;
+import it.unibo.oop.lab.socialnetwork.User;
+
 /**
  * This is going to act as a test for
  * {@link it.unibo.oop.lab.enum1.SportSocialNetworkUserImpl}
@@ -25,5 +28,13 @@ public final class TestSportByEnumeration {
          * By now, you know how to do it
          */
         // TODO
+    	final SportSocialNetworkUserImpl<User> dcassani = new SportSocialNetworkUserImpl<User>("Davide","Cassani" , "dcassani", 53);
+        final SportSocialNetworkUserImpl<User> becclestone = new SportSocialNetworkUserImpl<User>("Bernie","Ecclestone" , "becclestone", 83);
+        
+        dcassani.addSport(Sport.BIKE);
+        dcassani.addSport(Sport.MOTOGP);
+        System.out.println("Cassani has been a professional biker: "
+                + dcassani.hasSport(Sport.BIKE));
+        System.out.println("Cassani does not like soccer: " + !dcassani.hasSport(Sport.SOCCER));
     }
 }
